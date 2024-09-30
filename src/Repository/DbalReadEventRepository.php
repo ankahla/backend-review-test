dd6a25c5d7d30f34dd6f86067e774c8196262692
Anouar Kahla <anouar@latactik.com>
2024-09-30 14:57:27 +0200

    fix: sql syntax error

--- src/Repository/DbalReadEventRepository.php
+++ src/Repository/DbalReadEventRepository.php
@@ -17,11 +17,12 @@ class DbalReadEventRepository implements ReadEventRepository
         SELECT sum(count) as count
         FROM event
         WHERE date(create_at) = :date
-        AND payload like %{$searchInput->keyword}%
+        AND payload::text like :keyword
 SQL;
 
         return (int) $this->connection->fetchOne($sql, [
-            'date' => $searchInput->date,
+            'date' => $searchInput->date->format('Y-m-d H:i:s'),
+            'keyword' => "%{$searchInput->keyword}%",
         ]);
     }
 
@@ -31,12 +32,13 @@ SQL;
             SELECT type, sum(count) as count
             FROM event
             WHERE date(create_at) = :date
-            AND payload like %{$searchInput->keyword}%
+            AND payload::text like :keyword
             GROUP BY type
 SQL;
 
         return $this->connection->fetchAllKeyValue($sql, [
-            'date' => $searchInput->date,
+            'date' => $searchInput->date->format('Y-m-d H:i:s'),
+            'keyword' => "%{$searchInput->keyword}%",
         ]);
     }
 
@@ -46,13 +48,13 @@ SQL;
             SELECT extract(hour from create_at) as hour, type, sum(count) as count
             FROM event
             WHERE date(create_at) = :date
-            AND payload like %{$searchInput->keyword}%
+            AND payload::text like :keyword
             GROUP BY TYPE, EXTRACT(hour from create_at)
 SQL;
 
-        /** @phpstan-ignore-next-line */
-        $stats = $this->connection->fetchAll($sql, [
-            'date' => $searchInput->date,
+        $stats = $this->connection->fetchAllAssociative($sql, [
+            'date' => $searchInput->date->format('Y-m-d H:i:s'),
+            'keyword' => "%{$searchInput->keyword}%",
         ]);
 
         $data = array_fill(0, 24, ['commit' => 0, 'pullRequest' => 0, 'comment' => 0]);
@@ -67,24 +69,16 @@ SQL;
     public function getLatest(SearchInput $searchInput): array
     {
         $sql = <<<SQL
-            SELECT type, repo
+            SELECT type
             FROM event
             WHERE date(create_at) = :date
-            AND payload like %{$searchInput->keyword}%
+            AND payload::text like :keyword
 SQL;
 
-        $result = $this->connection->fetchAllAssociative($sql, [
-            'date' => $searchInput->date,
-            'keyword' => $searchInput->keyword,
+        return $this->connection->fetchAllAssociative($sql, [
+            'date' => $searchInput->date->format('Y-m-d H:i:s'),
+            'keyword' => "%{$searchInput->keyword}%",
         ]);
-
-        $result = array_map(static function ($item) {
-            $item['repo'] = json_decode((string) $item['repo'], true);
-
-            return $item;
-        }, $result);
-
-        return $result;
     }
 
     public function exist(int $id): bool
